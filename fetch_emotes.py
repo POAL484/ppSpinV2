314e364527a634f4f89408b486b3f4db31d1f159
POAL <pbraginets48@gmail.com>
2026-05-22 22:49:08 +0300

    more emts fetch debugging!!! yeah!

--- fetch_emotes.py
+++ fetch_emotes.py
@@ -22,18 +22,22 @@ def get_named_platform(emote_object: dict): return {emote_object['name']: emote_
 
 async def fetch_7tv(twitch_id: int, bot):
     #resp = req.get(f"https://7tv.io/v3/emote-sets/{set_id}").json()
-    await bot.logger("before req")
+    await bot.logger("tryna 7tv")
     resp = req.get(f"https://7tv.io/v3/users/twitch/{twitch_id}", proxies=proxies).json()
-    await bot.logger("after req " + str(resp)[:30])
+    await bot.logger("7tv:  " + str(resp)[:30])
     return list(map(get_7tv_emote, resp['emote_set']['emotes']))
 
-def fetch_bttv(twitch_id: int):
+async def fetch_bttv(twitch_id: int, bot):
+    await bot.logger("tryna bttv")
     bttv =  req.get(f"https://api.betterttv.net/3/cached/users/twitch/{twitch_id}", proxies=proxies).json()
+    await bot.logger("bttv:  " + str(bttv)[:30])
     if 'message' in bttv.keys(): return []
     return list(map(get_bttv_emote, bttv['channelEmotes']))
 
-def fetch_ffz(twitch_id: int):
+async def fetch_ffz(twitch_id: int, bot):
+    await bot.logger("tryna ffz")
     ffzEmotes = req.get(f"https://api.frankerfacez.com/v1/room/id/{twitch_id}", proxies=proxies).json()
+    await bot.logger("ffz:  " + str(ffzEmotes)[:30])
     if 'error' in ffzEmotes.keys(): return []
     ffzEmotes = ffzEmotes['sets'][list(ffzEmotes['sets'].keys())[0]]['emoticons']
     return list(map(get_ffz_emote, ffzEmotes))
@@ -45,11 +49,11 @@ async def fetch_channel(bot, twitch_id: int):
     except Exception as e:
         await bot.logger(f"Не удалось 7tv обновить эмоуты {twitch_id}: {e}")
     try:
-        emts.extend(fetch_bttv(twitch_id))
+        emts.extend(await fetch_bttv(twitch_id, bot))
     except Exception as e:
         await bot.logger(f"Не удалось bttv обновить эмоуты {twitch_id}: {e}")
     try:
-        emts.extend(fetch_ffz(twitch_id))
+        emts.extend(await fetch_ffz(twitch_id, bot))
     except Exception as e:
         await bot.logger(f"Не удалось ffz обновить эмоуты {twitch_id}: {e}")
     await bot.logger(str(emts[:12]))
